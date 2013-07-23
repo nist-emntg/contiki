38140f477f70e1b8974b77cf35b088acc8987cfe
Tony Cheneau <tony.cheneau@nist.gov>
2013-07-23 17:30:35 -0400

    Add the NIST licence text and the name of the authors.

--- platform/native/dev/udpradio.c
+++ platform/native/dev/udpradio.c
@@ -1,3 +1,30 @@
+/*
+* Conditions Of Use
+*
+* This software was developed by employees of the National Institute of
+* Standards and Technology (NIST), and others.
+* This software has been contributed to the public domain.
+* Pursuant to title 15 United States Code Section 105, works of NIST
+* employees are not subject to copyright protection in the United States
+* and are considered to be in the public domain.
+* As a result, a formal license is not needed to use this software.
+*
+* This software is provided "AS IS."
+* NIST MAKES NO WARRANTY OF ANY KIND, EXPRESS, IMPLIED
+* OR STATUTORY, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTY OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT
+* AND DATA ACCURACY. NIST does not warrant or make any representations
+* regarding the use of the software or the results thereof, including but
+* not limited to the correctness, accuracy, reliability or usefulness of
+* this software.
+*/
+
+/* portion of this code was borrowed from the Cooja driver
+ * authors:
+ * - Ranganathan Mudumbai <mranga@nist.gov>
+ * - Tony Cheneau <tony.cheneau@nist.gov>
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <semaphore.h>
@@ -51,11 +78,11 @@ struct Node {
 static struct Node* recv_queue = NULL;
 
 PROCESS(udpradio, "udpradio process");
-static int
-radio_read(void *buf, unsigned short bufsize);
+static int radio_read(void *buf, unsigned short bufsize);
 void* radio_thread(void *args);
 
-/*---------------------------------------------------------------------------*/PROCESS_THREAD(udpradio, ev, data) {
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(udpradio, ev, data) {
 
 	PROCESS_BEGIN()
 	;
