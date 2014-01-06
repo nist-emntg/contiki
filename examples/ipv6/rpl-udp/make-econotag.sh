48f72f3bb8ba66064871b1caa516afb7c078ad84
Tony Cheneau <tony.cheneau@nist.gov>
2014-01-06 15:02:56 -0500

    Improve the build system to burn fix addresses in the econotag.
    The new build system generates multiple images for the client, each of
    them with a different MAC address.

--- examples/ipv6/rpl-udp/make-econotag.sh
+++ examples/ipv6/rpl-udp/make-econotag.sh
@@ -1,4 +1,13 @@
+export MAX_ID=2
 make TARGET=econotag clean &&\
-make TARGET=econotag UIP_CONF_TCP=0 CERTIFICATE=1 SERVER_REPLY=1  udp-client && \
-make TARGET=econotag UIP_CONF_TCP=0 CERTIFICATE=1 DODAG_ROOT=1 SERVER_REPLY=1 udp-server
-
+make DEFINES=M12_CONF_SERIAL=99 TARGET=econotag UIP_CONF_TCP=0 CERTIFICATE=1 DODAG_ROOT=1 SERVER_REPLY=1  udp-server &&\
+mv udp-server_econotag.bin udp-server_econotag.flash && \
+for i in `seq 1 $MAX_ID`; do
+    make TARGET=econotag clean &&\
+    make DEFINES=M12_CONF_SERIAL=$i TARGET=econotag UIP_CONF_TCP=0 CERTIFICATE=1 SERVER_REPLY=1  udp-client &&\
+    mv udp-client_econotag.bin udp-client_econotag-$i.flash
+done
+mv udp-server_econotag.flash udp-server_econotag.bin
+for i in `seq 1 $MAX_ID`; do
+    mv udp-client_econotag-$i.flash udp-client_econotag-$i.bin
+done
